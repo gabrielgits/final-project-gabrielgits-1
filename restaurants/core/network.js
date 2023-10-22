1991e640d08f179eba951b75aa0a4b28014b5f31
Buyantugs <33389915+Buyantugs@users.noreply.github.com>
2023-10-22 13:01:57 -0500

    done food network part done but not tested

--- restaurants/core/network.js
+++ restaurants/core/network.js
@@ -3,45 +3,6 @@ import { constServer } from "./constats";
 
 export default {};
 
-// function formatDateToMMDDYYYY(date) {
-//   const mm = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
-//   const dd = String(date.getDate()).padStart(2, "0");
-//   const yyyy = date.getFullYear();
-
-//   return `${mm}-${dd}-${yyyy}`;
-// }
-
-// let foods = [
-//   {
-//     name: "Apple",
-//     origin: "Vietnam",
-//     price: 10,
-//     date: formatDateToMMDDYYYY(new Date()),
-//     image: "https://picsum.photos/200",
-//   },
-//   {
-//     name: "Orange",
-//     origin: "China",
-//     price: 8,
-//     date: formatDateToMMDDYYYY(new Date()),
-//     image: "https://picsum.photos/200",
-//   },
-//   {
-//     name: "Watermelon",
-//     origin: "USA",
-//     price: 6,
-//     date: formatDateToMMDDYYYY(new Date()),
-//     image: "https://picsum.photos/200",
-//   },
-//   {
-//     name: "Rise",
-//     origin: "Mongolia",
-//     price: 2,
-//     date: formatDateToMMDDYYYY(new Date()),
-//     image: "https://picsum.photos/200",
-//   },
-// ];
-
 export async function login(email, password) {
   try {
     const ret = await fetch(constServer + "/login", {
@@ -81,7 +42,7 @@ export async function signup(user) {
 
 export async function getFoodList(token, userId) {
   try {
-    const ret = await fetch(constServer+'/'+userId+'/foods', {
+    const ret = await fetch(constServer + '/' + userId + '/foods', {
       method: "GET",
       headers: {
         "content-type": "application/json",
@@ -96,17 +57,18 @@ export async function getFoodList(token, userId) {
   }
 }
 
-export async function addFood(food) {
+export async function addFood(food, token, userId) {
   try {
-    const ret = await fetch(constServer+'/foods', {
+    const ret = await fetch(constServer + '/' + userId + '/foods', {
       method: "POST",
       body: food,
       headers: {
-        "content-type": "application/json"
+        "content-type": "application/json",
+        'Authorization': `Bearer ${token}`
       }
     });
     const obj = await ret.json();
-    console.log(obj)    
+    console.log(obj)
     return { success: true };
   } catch (error) {
     throw error;
@@ -114,30 +76,33 @@ export async function addFood(food) {
 }
 
 
-export async function deleteFood(foodId) {
+export async function deleteFood(foodId, token, userId) {
   try {
-    // const ret = await fetch("http://localhost:5005/departments/653156f7b20ca0c4152c373a/courses", {
-    //   method: "DELETE",
-    //   headers: {
-    //     "content-type": "application/json"
-    //   }
-    // });
-    // const obj = await ret.json();
+    const ret = await fetch(constServer + '/' + userId + '/foods/'+foodId, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const obj = await ret.json();
     return { success: true };
   } catch (error) {
     throw error;
   }
 }
 
-export async function editFood(foodId) {
+export async function editFood(food, token, userId) {
   try {
-    // const ret = await fetch("http://localhost:5005/departments/653156f7b20ca0c4152c373a/courses", {
-    //   method: "PUT",
-    //   headers: {
-    //     "content-type": "application/json"
-    //   }
-    // });
-    // const obj = await ret.json();
+    const ret = await fetch(constServer + '/' + userId + '/foods/'+food._id, {
+      method: "PUT",
+      body: food,
+      headers: {
+        "content-type": "application/json",
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const obj = await ret.json();
     return { success: true };
   } catch (error) {
     throw error;
@@ -146,7 +111,7 @@ export async function editFood(foodId) {
 
 
 
-const BASE_URL = 'http://localhost:5001/users/65343946e323fd94f34b4790'; 
+const BASE_URL = 'http://localhost:5001/users/65343946e323fd94f34b4790';
 // const user_Id = "65343946e323fd94f34b4790";
 
 // network.js
@@ -159,45 +124,45 @@ export const addNoteToDB = async (noteData, token) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`, 
+        'Authorization': `Bearer ${token}`,
       },
       body: JSON.stringify(noteData),
     });
 
     if (response.ok) {
-      return true; 
+      return true;
     } else {
       return false;
     }
   } catch (error) {
     console.error('An error occurred:', error);
-    return false; 
+    return false;
   }
 };
-  export async function getDailyNotes(token) {
-    try {
-      const uri = `${BASE_URL}/notes`;
-      const headers = {
-        Authorization: `Bearer ${token}`, 
-      };
-      
-      const response = await fetch(uri, {
-        method: 'GET',
-        headers: headers,
-      });
-      
-      if (response.ok) {
-        const obj = await response.json();
-        console.log(obj);
-        return obj.data;
-      } else {
-        console.log('Request failed with status:', response.status);
-      }
-    } catch (error) {
-      console.log("Error:", error);
+export async function getDailyNotes(token) {
+  try {
+    const uri = `${BASE_URL}/notes`;
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await fetch(uri, {
+      method: 'GET',
+      headers: headers,
+    });
+
+    if (response.ok) {
+      const obj = await response.json();
+      console.log(obj);
+      return obj.data;
+    } else {
+      console.log('Request failed with status:', response.status);
     }
+  } catch (error) {
+    console.log("Error:", error);
   }
-export async function editDailyNote(editedNote, token){
+}
+export async function editDailyNote(editedNote, token) {
   try {
     const response = await fetch(`${BASE_URL}/notes/65347a46c8d17111ccc2f296`, {
       method: 'PATCH',
@@ -209,34 +174,34 @@ export async function editDailyNote(editedNote, token){
     });
 
     if (response.ok) {
-      return true; 
+      return true;
     } else {
-      return false; 
+      return false;
     }
   } catch (error) {
     console.error('An error occurred:', error);
-    return false; 
+    return false;
   }
 };
 
-  export async function deleteNote(tobeDeleted, token){
-    try {
-      const response = await fetch(`${BASE_URL}/notes/6534b30543b15dc39b3eb7ed`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify(tobeDeleted),
-      });
-  
-      if (response.ok) {
-        return true; 
-      } else {
-        return false; 
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-      return false; 
+export async function deleteNote(tobeDeleted, token) {
+  try {
+    const response = await fetch(`${BASE_URL}/notes/6534b30543b15dc39b3eb7ed`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(tobeDeleted),
+    });
+
+    if (response.ok) {
+      return true;
+    } else {
+      return false;
     }
-  };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return false;
+  }
+};
